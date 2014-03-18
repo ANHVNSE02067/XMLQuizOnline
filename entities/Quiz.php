bba2e9060899463414cf61cc88711f9472428543
huanpn <phanngoc_huan92@yahoo.com>
2014-03-18 21:19:02 +0700

    add quizModel

--- entities/Quiz.php
+++ entities/Quiz.php
@@ -2,15 +2,51 @@
 
 class Quiz 
 {
+    private $_quizID;
+    private $_staffID;
+    private $_is_closed;
     private $_descriptionQuiz;
     private $_time;
     private $_questions;
     
-    public function __construct($descriptionQuiz = null, $time = null, $questions = array()) {
+    public function __construct($quizID = null, $staffID = null, $is_closed = null, $descriptionQuiz = null, $time = null, $questions = array()) {
+        $this->_quizID = $quizID;
+        $this->_staffID = $staffID;
+        $this->_is_closed = $is_closed;
         $this->_descriptionQuiz = $descriptionQuiz;
         $this->_time = $time;
         $this->_questions = $questions;
     }
+    
+    public function setQuizID($quizID)
+    {
+        $this->_quizID = $quizID;
+    }
+
+    public function getQuizID()
+    {
+        return $this->_quizID;
+    }   
+    
+    public function setStaffID($staffID)
+    {
+        $this->_staffID = $staffID;
+    }
+
+    public function getStaffID()
+    {
+        return $this->_staffID;
+    }   
+    
+    public function setIsClosed($is_closed)
+    {
+        $this->_is_closed = $is_closed;
+    }
+
+    public function getIsClosed()
+    {
+        return $this->_is_closed;
+    }   
     
     public function setDescriptionQuiz($descriptionQuiz)
     {
---
+++ models/QuizModel.php
@@ -0,0 +1,29 @@
+<?php
+class QuizModel extends Model
+{
+    public function __construct() {
+        parent::__construct('Quiz.xml');
+    }
+    
+    public function getAllQuiz() {
+        $xpath = $this->getXpath();
+        $query = "/quiz";
+        $elements = $xpath->query($query);
+        if ($elements->length() == 0){
+            return false;
+        }
+        $quizzes = array();
+        foreach ($elements as $element) {
+            $quiz[] = new Quiz(
+                    $element->getAttribute('quizID'),
+                    $element->getAttribute('staffID'),
+                    $element->getAttribute('is_closed'),
+                    $element->getElementsByTagName('descriptionQuiz')->item(0)->nodeValue,
+                    $element->getElementsByTagName('time')->item(0)->nodeValue,
+                    $element->getElementsByTagName('questions')->item(0)->nodeValue
+            );
+        }
+        return $quizzes;
+    }
+}
+
