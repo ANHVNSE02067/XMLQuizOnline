7bab7d542561bddeb4a299c0d7c8b30ea9de2a2a
ANHVNSE02067 <anhvnse02067@fpt.edu.vn>
2014-03-18 22:11:09 +0700

    Commit before merge

--- models/QuizModel.php
+++ models/QuizModel.php
@@ -4,7 +4,7 @@ class QuizModel extends Model
     public function __construct() {
         parent::__construct('Quiz.xml');
     }
-    
+
     public function getAllQuiz() {
         $xpath = $this->getXpath();
         $query = "/quiz";
@@ -15,15 +15,34 @@ class QuizModel extends Model
         $quizzes = array();
         foreach ($elements as $element) {
             $quiz[] = new Quiz(
-                    $element->getAttribute('quizID'),
-                    $element->getAttribute('staffID'),
-                    $element->getAttribute('is_closed'),
-                    $element->getElementsByTagName('descriptionQuiz')->item(0)->nodeValue,
-                    $element->getElementsByTagName('time')->item(0)->nodeValue,
-                    $element->getElementsByTagName('questions')->item(0)->nodeValue
+                $element->getAttribute('quizID'),
+                $element->getAttribute('staffID'),
+                $element->getAttribute('is_closed'),
+                $element->getElementsByTagName('descriptionQuiz')->item(0)->nodeValue,
+                $element->getElementsByTagName('time')->item(0)->nodeValue,
+                $element->getElementsByTagName('questions')->item(0)->nodeValue
             );
         }
         return $quizzes;
+    }
+
+    public function getQuizById() 
+    {
+        $xpath = $this->getXpath();
+        $query = "//quiz";
+        $elements = $xpath->query($query);
+        if ($elements->length === 0) {
+            return false;
+        }
+        $quizNode = $elements->item(0);
+        $quiz = new Quiz(
+            $element->getAttribute('quizID'),
+            $element->getAttribute('staffID'),
+            $element->getAttribute('is_closed'),
+            $element->getElementsByTagName('descriptionQuiz')->item(0)->nodeValue,
+            $element->getElementsByTagName('time')->item(0)->nodeValue,
+            $element->getElementsByTagName('questions')->item(0)->nodeValue
+        );
     }
 }
 
